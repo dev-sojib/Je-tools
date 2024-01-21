cbcabbfef2137d93584ab462025858038c05ff5a
dev-sojib <dmsojib2002@gmail.com>
2024-01-21 16:13:46 +0600

    Like button done

---
+++ Like button like instragram/app.js
@@ -0,0 +1,14 @@
+let con = document.querySelector("#container")
+let love = document.querySelector("i")
+
+con.addEventListener('dblclick', function () {
+    love.style.opacity = '0.8'
+    love.style.transform = 'translate(-50%,-50%) scale(1)'
+    setTimeout(function () {
+        love.style.opacity = '0'
+    }, 1000)
+
+    setTimeout(function () {
+        love.style.transform = 'translate(-50%,-50%) scale(0)'
+    }, 2000)
+})
