bc88e5bcd4e368d76be9e61e1285e83c9f5b027c
dev-sojib <dmsojib2002@gmail.com>
2024-01-21 17:11:27 +0600

    Custom cursor added

---
+++ Custom cursor/app.js
@@ -0,0 +1,7 @@
+let main = document.querySelector('#main')
+let cursor = document.querySelector('.cursor')
+
+main.addEventListener('mousemove',function(event){
+    cursor.style.left = event.x+"px"
+    cursor.style.top = event.y+"px"
+})
